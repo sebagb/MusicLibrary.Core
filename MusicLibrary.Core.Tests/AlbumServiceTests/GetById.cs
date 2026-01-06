d1ca3fdda9d8f89bcc18c5f6cfe806332eb65051
sebagb <sgbcmpu@gmail.com>
2026-01-06 10:09:44 +0100

    Assert album collections when GetById

--- MusicLibrary.Core.Tests/AlbumServiceTests/GetById.cs
+++ MusicLibrary.Core.Tests/AlbumServiceTests/GetById.cs
@@ -28,4 +28,85 @@ public class GetById
 
         Assert.NotNull(album);
     }
+
+    [Fact]
+    public void CountriesCollectionIsEmptyIfAlbumHasNoCountries()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums();
+        var service = new AlbumService(repo);
+
+        var album = service.GetById(1111);
+
+        var albumCountries = album!.Countries;
+        Assert.Empty(albumCountries);
+    }
+
+    [Fact]
+    public void CountriesAreLoaded()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums()
+            .WithCountries();
+        var service = new AlbumService(repo);
+
+        var album = service.GetById(1111);
+
+        var albumCountries = album!.Countries;
+        Assert.NotEmpty(albumCountries);
+    }
+
+    [Fact]
+    public void GenresCollectionIsEmptyIfAlbumHasNoGenres()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums();
+        var service = new AlbumService(repo);
+
+        var album = service.GetById(1111);
+
+        var albumGenres = album!.Genres;
+        Assert.Empty(albumGenres);
+    }
+
+    [Fact]
+    public void GenresAreLoaded()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums()
+            .WithGenres();
+        var service = new AlbumService(repo);
+
+        var album = service.GetById(1111);
+
+        var albumGenres = album!.Genres;
+        Assert.NotEmpty(albumGenres);
+    }
+
+    [Fact]
+    public void StylesCollectionIsEmptyIfAlbumHasNoStyles()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums();
+        var service = new AlbumService(repo);
+
+        var album = service.GetById(1111);
+
+        var albumStyles = album!.Styles;
+        Assert.Empty(albumStyles);
+    }
+
+    [Fact]
+    public void StylesAreLoaded()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums()
+            .WithStyles();
+        var service = new AlbumService(repo);
+
+        var album = service.GetById(1111);
+
+        var albumStyles = album!.Styles;
+        Assert.NotEmpty(albumStyles);
+    }
 }
