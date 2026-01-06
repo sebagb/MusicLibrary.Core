9b6bff5a16106e55cc03d5c978218755b1cf4874
sebagb <sgbcmpu@gmail.com>
2026-01-06 09:32:12 +0100

    Test empty collections for GetAll method

--- MusicLibrary.Core.Tests/AlbumServiceTests/GetAll.cs
+++ MusicLibrary.Core.Tests/AlbumServiceTests/GetAll.cs
@@ -5,6 +5,20 @@ namespace MusicLibrary.Core.Tests.AlbumServiceTests;
 public class GetAll
 {
     [Fact]
+    public void CountriesCollectionIsEmptyIfAlbumHasNoCountries()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums();
+        var service = new AlbumService(repo);
+
+        var collection = service.GetAll();
+
+        var firstAlbum = collection.First();
+        var albumCountries = firstAlbum.Countries;
+        Assert.Empty(albumCountries);
+    }
+
+    [Fact]
     public void CountryNamesAreLoadedForEveryAlbum()
     {
         var repo = new AlbumRepositoryMock()
@@ -20,6 +34,20 @@ public class GetAll
     }
 
     [Fact]
+    public void GenresCollectionIsEmptyIfAlbumHasNoGenres()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums();
+        var service = new AlbumService(repo);
+
+        var collection = service.GetAll();
+
+        var firstAlbum = collection.First();
+        var albumGenres = firstAlbum.Genres;
+        Assert.Empty(albumGenres);
+    }
+
+    [Fact]
     public void GenreNamesAreLoadedForEveryAlbum()
     {
         var repo = new AlbumRepositoryMock()
@@ -32,6 +60,20 @@ public class GetAll
         var firstAlbum = collection.First();
         var albumGenres = firstAlbum.Genres;
         Assert.Single(albumGenres);
+    }
+
+    [Fact]
+    public void StylesCollectionIsEmptyIfAlbumHasNoStyles()
+    {
+        var repo = new AlbumRepositoryMock()
+            .WithAlbums();
+        var service = new AlbumService(repo);
+
+        var collection = service.GetAll();
+
+        var firstAlbum = collection.First();
+        var albumStyles = firstAlbum.Styles;
+        Assert.Empty(albumStyles);
     }
 
     [Fact]
